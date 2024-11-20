b6a80a427df53f1a40cbb97ef1172db23f96b298
piat <yonathan.piat@gmail.com>
2024-11-20 15:53:51 +0100

    ajout exos 1 part 2 de 2023

--- 2023/1.py
+++ 2023/1.py
@@ -32,3 +32,59 @@ print(count)
     
             
 ## part 2
+
+chaine3 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+#listinstruction = chaine3.splitlines()
+count = 0
+for ligne in listinstruction:
+    first = 0
+    last = 0
+    compteur = 0
+    for letter in ligne:
+        
+        if letter == "o":
+            if ligne[compteur + 1 : compteur + 2+1] == "ne":
+                letter = "1"
+        elif letter == "t":
+            #print(ligne[compteur + 1 : compteur + 2+1])
+            if ligne[compteur + 1 : compteur + 2+1] == "wo":
+                #print("ok")
+                letter = "2"
+            if ligne[compteur + 1 : compteur + 4+1] == "hree":
+                letter = "3"
+        elif letter == "f":
+            if ligne[compteur + 1 : compteur + 3+1] == "our":
+                letter = "4"
+            if ligne[compteur + 1 : compteur + 3+1] == "ive":
+                letter = "5"
+        elif letter == "s":
+            if ligne[compteur + 1 : compteur + 2+1] == "ix":
+                letter = "6"
+            if ligne[compteur + 1 : compteur + 4+1] == "even":
+                letter = "7"
+        elif letter ==  "e": 
+            if ligne[compteur + 1 : compteur + 4+1] == "ight":
+                letter = "8"
+        elif letter ==  "n": 
+            if ligne[compteur + 1 : compteur + 3+1] == "ine":
+                letter = "9"
+
+
+        if isAdigit(letter):
+            last = letter
+            if first == 0:
+                first = letter
+        
+        
+        compteur += 1
+    #print(first+last)
+    count += int(first+last) # first et last sont bien des chaines de caractere donc cest une concatenation pas de piege normalement
+
+print(count)
