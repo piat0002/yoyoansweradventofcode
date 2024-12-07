d5885f774c60053ef68a39bcca75b059b6e33ebd
piat <yonathan.piat@gmail.com>
2024-12-07 14:17:50 +0100

    premiere prototype marche sur le test

--- 2024/jour4/code.js
+++ 2024/jour4/code.js
@@ -45,49 +45,195 @@ function createArrayInputLetter(input){
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function isInRange(indexLigne,indexCol,arrayInputlength,ligneLength){
-    return indexLigne < 0 & indexLigne < 0 & indexLigne < arrayInputlength && indexCol < ligneLength;
+    return indexLigne >= 0 && indexCol >= 0 && indexLigne < arrayInputlength && indexCol < ligneLength;
 }
 
-function srcheachXmax(arrayInputLetter,indexLigne,indexCol){
+function srcheachXmax(arrayInputLetter,indexLigne,indexCol,letterIndex){
     if (arrayInputLetter == "X") {
         
     }
 }
-verifAll(arrayInputLetter,indexLigne,indexCol){
-    let = count
+
+function colorToGreen(ligne,colonne){
+    let selector = `.l${ligne} > .c${colonne}`;
+    let elementEnTraitement = document.querySelector(selector);
+    elementEnTraitement.style.color = "green";
+}
+
+const Xmas = "XMAS"
+function verifAll(arrayInputLetter,indexLigne,indexCol){
     let arrayXmas = []
-    verifColBas(arrayInputLetter,indexLigne,indexCol)
-    verifColHaut(arrayInputLetter,indexLigne,indexCol)
-    verifDiaBasDer(arrayInputLetter,indexLigne,indexCol)
-    verifDiaBasDevant(arrayInputLetter,indexLigne,indexCol)
-    verifDiaHautDer(arrayInputLetter,indexLigne,indexCol)
-    verifDiaHautDevant(arrayInputLetter,indexLigne,indexCol)
-    verifLigneDer(arrayInputLetter,indexLigne,indexCol)
-    verifLigneDevant(arrayInputLetter,indexLigne,indexCol)
-    return [,count]
+    if(isInRange(indexLigne,indexCol,arrayInputLetter.length,arrayInputLetter[0].length) ){
+        if(arrayInputLetter[indexLigne][indexCol] != Xmas[0]){
+            return arrayXmas
+        }
+    }
+    //pas opti mais le code est simplifier
+    if (verifColBas(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("CB");
+    if (verifColHaut(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("CH");
+    if (verifDiaBasDer(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("DBD");
+    if (verifDiaBasDevant(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("DBV");
+    if (verifDiaHautDer(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("DHR");
+    if (verifDiaHautDevant(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("DHV");
+    if (verifLigneDer(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("LDR");
+    if (verifLigneDevant(arrayInputLetter,indexLigne,indexCol)) arrayXmas.push("LDV");
+    return arrayXmas
 }
 function verifLigneDevant(arrayInputLetter,indexLigne,indexCol){
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne;
+        let colonne = indexCol + i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+    }
+    for (let i = 0; i < 4; i++) {
+        let ligne = indexLigne;
+        let colonne = indexCol + i;
+        colorToGreen(ligne,colonne)
+    }
+    
+    return true
 }
 function verifLigneDer(arrayInputLetter,indexLigne,indexCol){
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne;
+        let colonne = indexCol - i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
 
+    }
+    for (let i = 0; i < 4; i++) {
+        let ligne = indexLigne;
+        let colonne = indexCol - i;
+        colorToGreen(ligne,colonne)
+    }
+    
+    return true
 }
 function verifDiaHautDevant(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne + i;
+        let colonne = indexCol - i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }
+    for (let i = 0; i < 4; i++) {
+        let ligne = indexLigne + i;
+        let colonne = indexCol - i;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 function verifDiaBasDevant(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne + i;
+        let colonne = indexCol + i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }
+    for (let i = 0; i < 4; i++) {
+        
+        let ligne = indexLigne + i;
+        let colonne = indexCol + i;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 function verifDiaBasDer(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne - i;
+        let colonne = indexCol + i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }
+    for (let i = 0; i < 4; i++) {
+        
+        let ligne = indexLigne - i;
+        let colonne = indexCol + i;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 function verifDiaHautDer(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne - i;
+        let colonne = indexCol - i;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }
+    for (let i = 0; i < 4; i++) {
+        
+        let ligne = indexLigne - i;
+        let colonne = indexCol - i;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 function verifColHaut(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne - i;
+        let colonne = indexCol;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }    
+    for (let i = 0; i < 4; i++) {
+        let ligne = indexLigne - i;
+        let colonne = indexCol;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 function verifColBas(arrayInputLetter,indexLigne,indexCol){
-
+    for (let i = 1; i < 4; i++) {
+        let ligne = indexLigne + i;
+        let colonne = indexCol;
+        if(!isInRange(ligne,colonne,arrayInputLetter.length,arrayInputLetter[0].length)){
+            return false
+        }
+        if(arrayInputLetter[ligne][colonne] != Xmas[i]){
+            return false
+        }
+        
+    }
+    for (let i = 0; i < 4; i++) {
+        let ligne = indexLigne + i;
+        let colonne = indexCol;
+        colorToGreen(ligne,colonne)
+    }
+    return true
 }
 async function Resolution(input){
     let compteur = 0; 
@@ -100,11 +246,18 @@ async function Resolution(input){
         for (let indexCol = 0; indexCol < ligneLength; indexCol++) {
             let selector = `.l${indexLigne} > .c${indexCol}`;
             let elementEnTraitement = document.querySelector(selector);
-            elementEnTraitement.style.color = 'blue';
+            let elementEnTraitementWasGreen = elementEnTraitement.style.color == "green"
+            elementEnTraitement.style.color = `${"blue"} `;
             arrayInputLetter[indexLigne][indexCol]
-            await delay(500);
+            await delay(300);
 
-            elementEnTraitement.style.color = `${elementEnTraitement.style.color == "green" ? "green" : "grey"} `;
+            
+            tableXmas = verifAll(arrayInputLetter,indexLigne,indexCol);
+            //console.log(tableXmas)
+            compteur += tableXmas.length;
+            console.log(elementEnTraitementWasGreen)
+            
+            elementEnTraitement.style.color = `${elementEnTraitementWasGreen || elementEnTraitement.style.color == "green"  ? "green" : "grey"} `;
         }
     }
     console.log(compteur);
